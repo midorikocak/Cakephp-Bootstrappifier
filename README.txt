ae07e6ab2df286073861f74a52c7b323d363b85b
Mutlu Tevfik Koçak <mtkocak@gmail.com>
2011-11-16 23:58:47 +0200

    Avesome files added

---
+++ README.txt
@@ -0,0 +1,20 @@
+
+Bootstrappifier for CakePHP is a Javascript library to use Twitter Bootstrap library with CakePHP without need of FormHelper hacks.
+
+For download:
+
+https://github.com/mtkocak
+
+For install:
+
+1. You have to have Twitter Bootstrap configured. (cake.generic.css removed from default.ctp, files downloaded and included to you webroot/css and web root/js folders )
+
+2. Copy cakebootstrap.js in your webroot/js folder 
+
+3. Your View/Layouts/default.ctp file must include this in head tag:
+
+echo $this->Html->css('bootstrap.min'); //You don't need to add again if you have this
+echo $this->Html->script('libs/jquery-1.6.2.min.js');
+
+
+- For any question mtkocak@gmail.com
---
+++ cakebootstrap.js
@@ -0,0 +1,71 @@
+/*
+* Twitter Bootstrappifier for CakePHP 
+*
+* Author: Mutlu Tevfik Kocak
+*
+* CakePHP Twitter Bootstrappifier
+*
+* Selects all con twitter Bootstrap incompatible form and buttons,
+* and converts them into pretty Twitter Bootstrap style.
+*
+*/
+
+function cakebootstrap()
+{
+//All submit forms wrapped to div.action
+$('input][type="submit"][class!="btn primary"]').wrap('<div class="actions" />');
+//All submit forms converted to primary button
+$('input[type="submit"]').addClass('btn primary');
+//All index actions converted into pretty buttons
+$('td][class="actions"] > a[class!="btn"]').addClass('btn');
+
+//All (div.inputs) with default FormHelper style (div.input > label ~ input)
+//converted into Twitter Bootstrap Style (div.clearfix > label ~ div.input)
+
+$('div[class!="input added"].input').removeClass().addClass('clearfix');
+$('div.clearfix > label ~ input').wrap('<div class="input added" />');
+$('div.clearfix > label ~ select').wrap('<div class="input added" />');
+
+
+}
+
+//Default CakePHP Error inputs are converted to twitter bootstrap style
+function errorstrap()
+{
+	$('.error-message').addClass('help-inline');
+	$('.form-error').addClass('error');
+	$('.form-error').closest('.clearfix').addClass('error');
+}
+
+//Styling start when document loads
+$(document).ready(function(){
+	cakebootstrap();
+	errorstrap();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
